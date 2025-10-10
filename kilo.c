3b6598b7fde5a25ebc69b1fb997b0c3a8af4ff88
Dorian Collier <ocorneillard@proton.me>
2025-10-10 14:36:46 +0200

    move cursor

--- kilo.c
+++ kilo.c
@@ -1,5 +1,4 @@
-/*** Append Buffer ***/
-/*** Still have to understand dynamic array ***/
+/*** Move the cursor <= loop and repeat ***/
 
 /*** includes ***/
 
@@ -13,6 +12,8 @@
 #include <unistd.h>
 
 /*** defines ***/
+
+#define VIF_VERSION "0.0.1"
 
 #define CTRL_KEY(k) ((k) & 0x1f)
 
@@ -121,24 +122,42 @@ void abFree(struct abuf *ab) {
 
 /*** output ***/
 
-void editorDrawRows() {
+void editorDrawRows(struct abuf *ab) {
   int y;
   for (y = 0; y < E.screenrows; y++) {
-    write(STDOUT_FILENO, "~", 1);
+    if (y == E.screenrows / 3) {
+      char welcome[80];
+      int welcomelen = snprintf(welcome, sizeof(welcome), "Vif editor -- version %s", VIF_VERSION);
+      if (welcomelen > E.screencols) welcomelen = E.screencols;
+      int padding = (E.screencols - welcomelen) / 2;
+      if (padding) {
+	abAppend(ab, "~", 1);
+	padding--;
+      }
+      while (padding--) abAppend(ab, " ", 1);
+      abAppend(ab, welcome, welcomelen);
+    }
+    abAppend(ab, "~", 1);
 
+    abAppend(ab, "\x1b[K", 3);
     if (y < E.screenrows - 1) {
-      write(STDOUT_FILENO, "\r\n", 2);
+      abAppend(ab, "\r\n", 2);
     }
   }
 }
 
 void editorRefreshScreen() {
-  write(STDOUT_FILENO, "\x1b[2J", 4);
-  write(STDOUT_FILENO, "\x1b[H", 3);
+  struct abuf ab = ABUF_INIT;
 
-  editorDrawRows();
+  abAppend(&ab, "\x1b[?25l", 6);
+  abAppend(&ab, "\x1b[H", 3);
 
-  write(STDOUT_FILENO, "\x1b[H", 3);
+  editorDrawRows(&ab);
+
+  abAppend(&ab, "\x1b[H", 3);
+  write(STDOUT_FILENO, ab.b, ab.len);
+  abAppend(&ab, "\x1b[?25h", 6);
+  abFree(&ab);
 }
 
 
