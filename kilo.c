d94d854acbbd7ed5140cb1b151cab23e2520009d
Dorian Collier <ocorneillard@proton.me>
2025-10-11 13:29:58 +0200

    line

--- kilo.c
+++ kilo.c
@@ -1,4 +1,4 @@
-/*** To fix ***/
+/*** A line viewer ***/
 
 /*** includes ***/
 
@@ -22,6 +22,9 @@ enum editorKey {
   ARROW_RIGHT,
   ARROW_UP,
   ARROW_DOWN,
+  DEL_KEY,
+  HOME_KEY,
+  END_KEY,
   PAGE_UP,
   PAGE_DOWN
 };
@@ -86,8 +89,13 @@ int editorReadKey() {
 	if (read(STDIN_FILENO, &seq[2], 1) != 1) return '\x1b';
 	if (seq[2] == '~') {
 	  switch (seq[1]) {
+	  case '1': return HOME_KEY;
+	  case '3': return DEL_KEY;
+	  case '4': return END_KEY;
 	  case '5': return PAGE_UP;
 	  case '6': return PAGE_DOWN;
+	  case '7': return HOME_KEY;
+	  case '8': return END_KEY;
 	  }
 	}
       } else {
@@ -96,10 +104,16 @@ int editorReadKey() {
 	case 'B': return ARROW_DOWN;
 	case 'C': return ARROW_RIGHT;
 	case 'D': return ARROW_LEFT;
+	case 'H': return HOME_KEY;
+	case 'F': return END_KEY;
 	}
       }
+    } else if (seq[0] == 'O') {
+      switch (seq[1]) {
+      case 'H': return HOME_KEY;
+      case 'F': return END_KEY;
+      }
     }
-
     return '\x1b';
   } else {
     return c;
@@ -239,6 +253,13 @@ void editorProcessKeypress() {
     write(STDOUT_FILENO, "\x1b[2J", 4);
     write(STDOUT_FILENO, "\x1b[H", 3);
     exit(0);
+    break;
+
+  case HOME_KEY:
+    E.cx = 0;
+    break;
+  case END_KEY:
+    E.cx = E.screencols - 1;
     break;
   case ARROW_UP:
   case ARROW_DOWN:
